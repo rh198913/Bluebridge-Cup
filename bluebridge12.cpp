1556830af5474b19a0be55a1c86284e195bcb097
恬恬 <109257015+rh198913@users.noreply.github.com>
2022-11-24 21:40:54 +0800

    Update bluebridge12.cpp

--- bluebridge12.cpp
+++ bluebridge12.cpp
@@ -70,3 +70,77 @@ main()
    }
    cout << tol <<endl;
 }
+//年号字串
+/* int main()//计算出两位字符的数，后面遍历++
+{
+    int num = 702;
+    for(char x ='A';x<='Z';x++){
+        for(char y ='A';y<='Z';y++){
+            for(char z ='A';z<='Z';z++){
+                    num++;
+                 if (num == 2019)                
+                 cout<<x<<y<<z<<endl;                
+                            
+            }
+        }
+    }
+}  */
+//typedef long long ll;
+ /* int main()
+{
+    int a = 1;
+    int b = 1;
+    int c = 1;
+    int d = 0;
+    int n = 3;
+    {
+        while(n != 20190324){
+            n++;//只是为了退出循环   
+            d = (a+b+c)%10000;
+            a = b;
+            b = c;
+            c = d;
+        }
+        cout <<d<<endl;
+    }
+    return 0;
+} 
+ */
+/* int a[20190324];
+int main()
+{
+	a[0]=1,a[1]=1,a[2]=1;
+	for(int i=3;i<=20190324;i++)
+    {
+		a[i]=a[i-1]+a[i-2]+a[i-3];
+           a[i] %= 10000;
+    }
+	cout <<a[20190323]<<endl;
+	return 0;
+} */
+bool f(int x)
+{ 
+    int n = x;
+    while(n!=0)
+    {
+        if(n%10==2||n%10==4)
+            return false;
+            n/= 10;
+    }
+    return true;
+}
+int main()
+
+{
+    int n = 0;
+    for(int i = 1;i<2019;i++){
+        for(int j = i+1;j<2019;j++){
+            int k = 2019-i-j;
+            if(f(i)&&f(j)&&f(k)&&(j<k))
+            n++;
+    
+        }
+    }
+    cout<<n;
+    return 0;
+}
