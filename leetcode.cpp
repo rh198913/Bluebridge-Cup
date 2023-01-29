fba617ab1907b472f3c24549fd833da1eab07c1c
恬恬 <109257015+rh198913@users.noreply.github.com>
2023-01-29 18:26:07 +0800

    Add files via upload

---
+++ leetcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//哔站小猫学IT两数字和（无序数组）
+int main(){
+
+    int target = 13;
+    int arr[5] = {1,8,5,6,4};
+    for(int i = 0;i<5;i++){
+        for(int j = 0;j<5;j++){
+        if(arr[i] +arr[j] = target&&arr[i] != arr[j])
+        int a = i;
+        int b = j;
+        cout<<a<<endl;
+    }
+   }
+    return 0;
+}
+    
