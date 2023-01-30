b403c469a820ab96c5a098872d515e042ad7b38d
恬恬 <109257015+rh198913@users.noreply.github.com>
2023-01-30 18:55:43 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
 //哔站小猫学IT两数字和（无序数组）
+//第一题，两数字和（无序数组）
 int main(){
-
-    int target = 13;
-    int arr[5] = {1,8,5,6,4};
-    for(int i = 0;i<5;i++){
-        for(int j = 0;j<5;j++){
-        if(arr[i] +arr[j] = target&&arr[i] != arr[j])
-        int a = i;
-        int b = j;
-        cout<<a<<endl;
+    int arr[4] = {1,2,7,9};
+    int target = 10;
+    int z = sizeof(arr)/sizeof(arr[0]); 
+    int total[] = {1,2};
+    for(int i = 0;i<=z;i++){
+        for(int j = i+1;j<=z;j++){
+            int sum =arr[i]+arr[j];
+            if(sum == target){
+            total[0] = i;
+            total[1] = j;           
+        }
+       }
     }
-   }
+        cout<<total[0]<<endl;
+        cout<<total[1]<<endl;    
     return 0;
 }
     
